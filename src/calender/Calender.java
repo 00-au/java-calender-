d9cf87a256c2f0c101659cf4745c99c4e117d331
dmdgi <dmdgi@211.195.119.139>
2020-05-09 05:35:16 +0900

    update

--- src/calender/Calender.java
+++ src/calender/Calender.java
@@ -3,10 +3,12 @@ package calender;
 import java.util.Scanner;
 
 public class Calender {
-	
-	public static void main(String[] args) {
-		
-		System.out.println("Hello, Calender");
+	private final int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	public int maxDaysOfMonth(int month) {
+		return maxDays[month -1];
+	}
+
+	public void printSampleCalender() {
 		System.out.println(" 일  월   화   수   목   금   토");
 		System.out.println(" -------------------- ");
 		System.out.println(" 1  2  3  4  5  6  7 ");
@@ -14,13 +16,19 @@ public class Calender {
 		System.out.println(" 15 16 17 18 19 20 21 ");
 		System.out.println(" 22 23 24 25 26 27 28 ");
 	
+	}
+	
+	public static void main(String[] args) {
+		
+
 		Scanner scanner = new Scanner(System.in);
+		Calender cal = new Calender();
 		System.out.println("달을 입력하세요");
 		int month = scanner.nextInt();
 		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		System.out.printf("%d 월은 %d 일까지 있습니다.\n", month,cal.maxDaysOfMonth(month));
+		cal.printSampleCalender();
 		
-		System.out.printf("%d 월은 %d 일까지 있습니다.\n", month,maxDays[month - 1]);
 		scanner.close();
 	}
 }
